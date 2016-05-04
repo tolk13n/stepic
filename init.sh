f109bf5f2c4dbee8114472e56105f4ca55b030ae
tolk13n <t0lk@ukr.net>
2016-05-04 19:22:37 +0300

    no mint

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
-sudo ln -s /home/mint/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo rm /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default 
 sudo /etc/init.d/nginx restart
