9a10f9349c212b1823485894837123aaa6508419
tolk13n <t0lk@ukr.net>
2016-05-04 22:54:56 +0300

    last1

---
+++ hello.py
@@ -0,0 +1,9 @@
+def application(environ, start_response):
+    status = '200 OK'
+    headers = [('Content-Type','text/plain')]
+    body = str()
+    for i in environ['QUERY_STRING'].split('&'):
+        body += i+'\r\n'
+    body = bytes(body, encoding = 'utf-8')
+    start_response(status, headers)
+    return [body]
