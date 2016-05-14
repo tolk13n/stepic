7c1e963d76fa1b7d966be2acc1a9ee5e475a4119
tolk13n <t0lk@ukr.net>
2016-05-15 00:57:15 +0300

    4

--- etc/test.py
+++ etc/test.py
@@ -1,3 +1,7 @@
 bind = '0.0.0.0:8080'
 pythonpath = '/home/box/web/'
 workers = 4
+accesslog = '/home/box/web/etc/hello.log'
+loglevel = 'info'
+access_log_format = '%(h)s %(t)s %(q)s' 
+daemon = 'True'
--- init.sh
+++ init.sh
@@ -2,4 +2,4 @@ sudo rm /etc/nginx/sites-enabled/test.conf
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default 
 sudo /etc/init.d/nginx restart
-gunicorn --config /home/box/web/etc/test.py hello -D
+gunicorn --config /home/box/web/etc/test.py hello 
