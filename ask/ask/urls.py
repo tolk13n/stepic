aa518d867209d381572fdd9569d33dd9a79ce130
tolk13n <t0lk@ukr.net>
2016-05-15 22:11:18 +0300

    new life

---
+++ ask/ask/urls.py
@@ -0,0 +1,28 @@
+"""ask URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+
+urlpatterns = [
+#    url(r'^admin/', admin.site.urls),
+    url(r'^$','qa.views.test'),
+    url(r'^login/','qa.views.test',name='login'),
+    url(r'^signup/$','qa.views.test',name='signup'),
+    url(r'^question/(?P<id>\d+)/$', 'qa.views.test',name='question'),
+    url(r'^ask/$','qa.views.test',name='ask'),
+    url(r'^popular/$','qa.views.test',name='popular'),
+    url(r'^new/$','qa.views.test',name='new')
+]
--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 sudo rm /etc/nginx/sites-enabled/test.conf
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-sudo rm -rf /etc/nginx/sites-enabled/default 
+sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-gunicorn --config /home/box/web/etc/test.py hello 
+sudo rm /etc/gunicorn.d/ask.conf
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/ask.conf
+sudo /etc/init.d/gunicorn restart
