950de551e771b6845547e0e4f326c1ea2805bdd6
tolk13n <t0lk@ukr.net>
2016-05-15 22:51:53 +0300

    ))

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -20,9 +20,9 @@ urlpatterns = [
 #    url(r'^admin/', admin.site.urls),
     url(r'^$','qa.views.test'),
     url(r'^login/','qa.views.test',name='login'),
-    url(r'^signup/$','qa.views.test',name='signup'),
+    url(r'^signup/','qa.views.test',name='signup'),
     url(r'^question/(?P<id>\d+)/$', 'qa.views.test',name='question'),
-    url(r'^ask/$','qa.views.test',name='ask'),
-    url(r'^popular/$','qa.views.test',name='popular'),
-    url(r'^new/$','qa.views.test',name='new')
+    url(r'^ask/','qa.views.test',name='ask'),
+    url(r'^popular/','qa.views.test',name='popular'),
+    url(r'^new/','qa.views.test',name='new')
 ]
