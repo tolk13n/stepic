71c2ede41e14270d66bf1e9e345a1bb667e13d48
tolk13n <t0lk@ukr.net>
2016-05-18 23:46:01 +0300

    vievs01

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -18,11 +18,12 @@ from django.contrib import admin
 
 urlpatterns = [
 #    url(r'^admin/', admin.site.urls),
-    url(r'^$','qa.views.test'),
+    url(r'^(?P<id>\d*)$','qa.views.test'),
     url(r'^login/','qa.views.test',name='login'),
     url(r'^signup/','qa.views.test',name='signup'),
-    url(r'^question/(?P<id>\d+)/$', 'qa.views.test',name='question'),
+    url(r'^question/(?P<id>\d+)/$', 'qa.views.question' ,name='question'),
     url(r'^ask/','qa.views.test',name='ask'),
     url(r'^popular/','qa.views.test',name='popular'),
     url(r'^new/','qa.views.test',name='new')
 ]
+
--- ask/qa/views.py
+++ ask/qa/views.py
@@ -2,5 +2,19 @@
 
 # Create your views here.
 from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import render
+from .models import Question
+
 def test(request, *args,**kwargs):
-    return HttpResponse('OK')
+    return HttpResponse('()))-OK')
+
+def question(request, id):
+    try:
+        q = Question.objects.get(id=id)
+    except:
+        raise Http404
+    
+    a = Question.answer.text
+#    return HttpResponse('Question_OK!<br>'+q.title+'\n'+q.text)
+    return render(request,'qa/index.html',{'q' : q})
